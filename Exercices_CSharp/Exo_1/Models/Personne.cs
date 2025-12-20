2566fc6995f858a5b84410a739aa9739af976fcc
Verly1969 <eddyverly@icloud.com>
2025-12-20 12:31:26 +0100

    test 1

--- Exercices_CSharp/Exo_1/Models/Personne.cs
+++ Exercices_CSharp/Exo_1/Models/Personne.cs
@@ -6,5 +6,21 @@ namespace Exo_1.Models
 {
     internal class Personne
     {
+        public string Nom { get; set; } = string.Empty;
+        public string Prenom { get; set; } = string.Empty;
+        private DateTime _dateNaiss;
+
+        public DateTime DateNaissance
+        {
+            get => _dateNaiss;
+
+            set
+            {
+                DateTime maintenant = DateTime.Now;
+                DateTime dateTest = maintenant.AddYears(-18);
+                // contrôle si majeur
+
+            }
+        }
     }
 }
--- Exercices_CSharp/Exo_1/Program.cs
+++ Exercices_CSharp/Exo_1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Exo_1
+﻿using Exo_1.Models;
+
+namespace Exo_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DateTime maintenant = DateTime.Now;
+            Console.WriteLine($"{maintenant.Year}");
+            Console.WriteLine("Entrez l'année de votre naissance:");
+            int annee = int.Parse( Console.ReadLine()! );
+            Console.WriteLine("Entrez le mois de votre naissance:");
+            int mois = int.Parse( Console.ReadLine()! );
+            Console.WriteLine("Entrez le jour de votre naissance");
+            int jour = int.Parse( Console.ReadLine()! ) ;
+
+            DateTime test = new DateTime(annee, mois,jour);
+
+            if((maintenant.Year - 18) >= test.Year)
+            {
+                Console.WriteLine("Vous êtes majeur");
+            }
+            else
+            {
+                Console.WriteLine("Vous êtes mineur");
+            }
         }
     }
 }
