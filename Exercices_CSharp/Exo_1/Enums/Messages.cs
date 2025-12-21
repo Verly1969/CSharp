aa8e2eb8c3fbcc3a86608affb7132629a43bb092
Verly1969 <eddyverly@icloud.com>
2025-12-21 11:25:06 +0100

    Modification Courant, DisplayMessage, Message

---
+++ Exercices_CSharp/Exo_1/Enums/Messages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo_1.Enums
+{
+    public enum Messages
+    {
+        AMOUNT_SUP_ZERO
+    }
+}
--- Exercices_CSharp/Exo_1/Models/Courant.cs
+++ Exercices_CSharp/Exo_1/Models/Courant.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exo_1.Enums;
+using Exo_1.Structures;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,50 @@ namespace Exo_1.Models
 {
     internal class Courant
     {
+        private Messages amountSup = Messages.AMOUNT_SUP_ZERO;
+        public string Numero { get; set; } = string.Empty;
+        public double Solde { get; private set; }
+        public required Personne Titulaire { get; set; }
+
+        private double _ligneDeCredit;
+
+        public double LigneDeCredit
+        {
+            get => _ligneDeCredit;
+
+            set
+            {
+                MessageMontantSup0(value);
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            MessageMontantSup0(montant);
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            MessageMontantSup0(montant);
+            // check if the amount can be debited
+            while (montant > (Solde + _ligneDeCredit))
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// procedure to check if the amount is indeed greater than 0
+        /// </summary>
+        /// <param name="montant"></param>
+        private void MessageMontantSup0(double montant)
+        {
+            do
+            {
+                DisplayMessages.
+            }
+            while (montant <= 0);
+        }
     }
 }
