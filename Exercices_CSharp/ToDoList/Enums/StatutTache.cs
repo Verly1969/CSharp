f54a98041e0fb826635f1b63a240c7aace489f71
Verly1969 <eddyverly@icloud.com>
2025-12-23 12:06:16 +0100

    Init exo TodoList

---
+++ Exercices_CSharp/ToDoList/Enums/StatutTache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Enums
+{
+    internal enum StatutTache
+    {
+        EnAttente,
+        EnCours,
+        Terminee
+    }
+}
---
+++ Exercices_CSharp/ToDoList/Structures/Tache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Structures
+{
+    internal struct Tache
+    {
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreation { get; set; }
+    }
+}
